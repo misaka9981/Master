ffad5160724f9c0a6df5599fb8730de9167abff9
misaka9981 <932353913@qq.com>
2019-10-17 10:20:43 +0800

    'fix it'

--- DP/EditDistanceProblem.java
+++ DP/EditDistanceProblem.java
@@ -5,5 +5,20 @@ public class EditDistanceProblem{
         if (n==0)
             return m;
 
+        if(s1.charAt(m-1)==s2.charAt(n-1))
+            return editDistanceRecursion(s1,s2,m-1,n-1);
+
+        return 1 + Math.min(editDistanceRecursion(s1, s2, m, n-1), //Insert
+                Math.min(editDistanceRecursion(s1, s2, m-1, n), // Remove
+                        editDistanceRecursion(s1, s2, m-1, n-1))); //Replace
+    }
+
+    public static void main(String[] args) {
+        String s1 = "cat";
+        String s2 = "dog";
+        EditDistanceProblem ed = new EditDistanceProblem();
+        int res = ed.editDistanceRecursion(s1, s2, s1.length(), s2.length());
+        System.out.println("Minimum Edit Distance -(Recursion): " + res);
     }
 }
+
