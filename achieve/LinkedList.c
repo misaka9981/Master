418e8920b6113c6110e35abe5234a7ef7b15794f
misaka9981 <932353913@qq.com>
2020-03-14 20:30:39 +0800

    linkedList

---
+++ achieve/LinkedList.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node;
+
+Node * CreateNode(int value)
+{
+    Node *node =(Node *)malloc(sizeof(Node));
+    node->data = value;
+    node->next =NULL;
+    return node;
+}
+
+Node* LinkedListSearchByVal(Node *head,int value)
+{
+    while (head->data!=value)
+    {
+        head = head->next;
+    }
+    return head;
+    
+}
+
+Node* LinkedListSearchByIndex(Node* head,int index)
+{
+    for (int i = 0; i < index; i++)
+    {
+        head = head->next;
+    }
+    return head;
+    
+}
+
+void LinkedListInsertF(Node* head,int val)
+{
+    Node *tmpNode = (Node *)malloc(sizeof(Node));
+    tmpNode->data=val;
+    tmpNode->next=head->next;
+    head->next = tmpNode;
+}
+
+void LinkedListInsertR(Node* head,int val)
+{
+    Node *tmpNode = (Node *)malloc(sizeof(Node));
+    tmpNode->data=val;
+    Node *s = head;
+    while (s->next!=NULL)
+        s = s->next;
+    s->next=tmpNode;
+    tmpNode->next=NULL;
+    
+    
+}
+
+void LinkedListInsertM(Node* head,int val,int index)
+{
+    for (int i = 0; i < index; i++)
+    {
+        head = head->next;
+    }
+    Node *tmp = head->next;
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->next = tmp;
+    head->next = newNode;
+}
+
+Node* LinkedListDeleteF(Node* head)
+{
+    // Node *tmp;
+    // tmp = head->next;
+    // free(head);
+    // return tmp;
+    return head->next;
+}
+
+Node* LinkedListDeleteR(Node* head)
+{
+    Node *tmp = head;
+    while (tmp->next->next!=NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next=NULL;
+    return head;
+}
+
+Node* LinkedListDeleteByVal(Node* head,int val)
+{
+    while (head!=NULL)
+    {
+        if(head->data!=val)
+            break;
+        head = head->next;
+    }
+    Node *pre =head;
+    Node *cur = head;
+    while (cur!=NULL)
+    {
+        if(cur->data==val)
+        {
+            pre->next = cur->next;
+        }else
+        {
+            pre=cur;
+        }
+        cur = cur->next;
+        
+    }
+    return head;
+}   
+    
+    
+
+
+
+Node* LinkedListDeleteByPos(Node* head,int index)
+{
+    Node *tmp = head;
+    for (int i = 0; i < index-1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *tmp2 = tmp->next;
+    tmp->next = tmp2->next;
+    free(tmp2);
+    return head;
+}
+int main()
+{
+    Node* head = CreateNode(1);
+    LinkedListInsertR(head,2);
+    LinkedListInsertR(head,3);
+    LinkedListInsertR(head,4);
+    LinkedListInsertR(head,5);
+    LinkedListInsertR(head,6);
+    LinkedListInsertM(head,11,3);
+    head = LinkedListDeleteByPos(head,2);
+    //head = LinkedListDeleteByVal(head,3);
+    // Node* tmp = LinkedListSearchByVal(head,6);
+    // printf("%d",tmp->data);
+    while (head!=NULL)
+    {
+        printf("%d",head->data);
+        head = head->next;
+    }
+    
+    return 0;
+}
--- achieve/SeqList.c
+++ achieve/SeqList.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define SIZE 100
 
 typedef struct SeqList1
@@ -16,7 +17,6 @@ typedef struct SeqList2
 
 void initListArray(SeqList2 *l,int capacity)
 {
-    assert(l!=NULL);
     l->array = (int *)malloc(sizeof(int)*capacity);
     l->capacity = capacity;
     l->length=0;
@@ -24,16 +24,12 @@ void initListArray(SeqList2 *l,int capacity)
 
 void initListMemory(SeqList2 *l,int capacity)
 {
-    assert(l!=NULL);
     l->array = (int *)malloc(sizeof(int)*capacity);
     l->capacity = capacity;
     l->length=0;
 }
 
 void SeqListDestory(SeqList2 *l){
-    assert(l!=NULL);
-    assert(l->array!=NULL);
-    free(l->array);
     l->length = 0;
     l->capacity = 0;
 }
@@ -68,11 +64,57 @@ int insertR(SeqList2 *l,int value)
     l->length++;
 }
 
-int insertM(SeqList2 *l,int value)
+int insertM(SeqList2 *l,int pos,int value)
 {
     if (getLength(l)>=l->capacity)
         return 0;
-    l->array[l->length] = value;
+    for (int i = l->length; i > pos; i--)
+    {
+        l->array[i] = l->array[i-1];
+    }
+    l->array[pos] = value;
     l->length++;
 }
 
+int deleteF(SeqList2 *l)
+{
+    if(l==NULL)
+        return 0;
+    for (int i = 0; i < l->length-1; i++)
+    {
+        l->array[i]=l->array[i+1];
+    }
+    l->length--;  
+}
+
+int deleteR(SeqList2 *l)
+{
+    l->length--;
+}
+
+int deleteM(SeqList2 *l,int pos)
+{
+    for (int i = pos; i < l->length; i++)
+    {
+        l->array[i]=l->array[i+1];
+    }
+    l->length--;
+}
+
+int main()
+{
+    SeqList2 *sq= (SeqList2 *)malloc(sizeof(SeqList2));
+    initListMemory(sq,6);
+    insertR(sq,1);
+    insertR(sq,2);
+    insertR(sq,3);
+    insertR(sq,4);
+    insertR(sq,4);
+    insertM(sq,3,8);
+    for (int i = 0; i < sq->length; i++)
+    {
+        printf("%d",sq->array[i]);
+    }
+    
+    return 0;
+}
